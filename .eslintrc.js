3605c19e1cddc18cb23031ac82bd19b7d3a642b7
hweejae <hwejae25@gamil.com>
2023-04-06 21:55:31 +0900

    Feat. PNG파일 설정

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,35 +2,24 @@ module.exports = {
   env: {
     node: true,
     es2021: true,
-    jest: true,
-    "react-native/react-native": true,
   },
-  extends: ["airbnb", "airbnb/hooks", "plugin:prettier/recommended"],
+  extends: ["eslint:recommended", "plugin:react/recommended"],
   overrides: [],
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
-    ecmaFeatures: {
-      jsx: true,
-    },
   },
-  plugins: ["react", "prettier", "react-native"],
+  plugins: ["react", "react-native"],
   rules: {
-    "no-plusplus": ["error", { allowForLoopAfterthoughts: true }],
-    semi: "warn",
-    "no-unused-vars": "warn",
+    indent: ["error", 2],
+    quotes: ["error", "double"],
+    semi: ["warn", "always"],
     "import/no-extraneous-dependencies": "off",
     "react/prop-types": "off",
     "react/jsx-props-no-spreading": "off",
     "react/self-closing-comp": "off",
     "react/jsx-filename-extension": ["warn", { extensions: [".js"] }],
-    "no-param-reassign": 0,
     "react-hooks/exhaustive-deps": "off",
-    "prettier/prettier": [
-      "error",
-      {
-        endOfLine: "auto",
-      },
-    ],
+    "no-param-reassign": 0,
   },
 };
---
+++ assets/static.js
@@ -0,0 +1,9 @@
+import grayBrickColumn from "./bricks/gray_column.png";
+import grayBrickRow from "./bricks/gray_row.png";
+import grayBrickRectangle from "./bricks/gray_rectangle.png";
+
+export {
+  grayBrickColumn,
+  grayBrickRow,
+  grayBrickRectangle,
+};
